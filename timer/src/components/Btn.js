75f58d74cff04139fe46037f34be584e7494a157
medezzi <madou_zz@yahoo.fr>
2018-08-27 13:38:23 +0100

    add components

---
+++ timer/src/components/Btn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Btn(props) {
+
+  return (
+    <div className="Time">
+    <button className="Button">{props.type}</button>
+    </div>
+  );
+}
+export default Btn
