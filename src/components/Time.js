dbef97e7180eda1696b0cb92ccc10dd7d4ca361e
medezzi <madou_zz@yahoo.fr>
2018-09-04 14:13:27 +0100

    dynamic timer

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Time(props) {
   return (
-    <div className="Time">
-        <h1>{props.hms}</h1>
+    <div className="time">
+      <h1>{props.hms}</h1>
       <h5>{props.smh}</h5>
     </div>
   );
 }
-export default Time
+export default Time;
